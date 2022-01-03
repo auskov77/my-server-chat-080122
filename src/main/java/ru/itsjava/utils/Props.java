103c7a5d7e7e7dcec07eca9ed68c3738d50c051e
auskov77 <auskov77@yandex.ru>
2022-01-03 17:51:02 +0300

    Properties version 1

---
+++ src/main/java/ru/itsjava/utils/Props.java
@@ -0,0 +1,19 @@
+package ru.itsjava.utils;
+
+import lombok.SneakyThrows;
+
+import java.util.Properties;
+
+public class Props {
+    private final Properties properties;
+
+    @SneakyThrows
+    public Props(){
+        this.properties = new Properties();
+        properties.load(Props.class.getClassLoader().getResourceAsStream("applicaion.properties"));
+    }
+
+    public String getValue(String key){
+       return properties.getProperty(key);
+    }
+}
